8d6bb902a1219d804c76f023a597a12295128e12
Nlrvanaflame <skeranov666@gmail.com>
2023-09-13 17:47:55 +0300

    Bug fix

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import app from './app';
 import sequelize from './config/dbConfig';
 const PORT = 4000;
 
-sequelize.sync({ force: true })
+sequelize.sync()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
